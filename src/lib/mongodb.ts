af28a11d46a066f66da9265ff2d9a33ee529b438
Lalit Yelane <lalityelane94@gmail.com>
2026-04-11 01:09:27 +0530

    fix: remove eslint from next config

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI is not defined in .env.local");
-}
-
-// Global cache to prevent multiple connections in dev
 declare global {
   var mongoose: {
     conn: mongoose.Connection | null;
@@ -19,15 +14,17 @@ if (!global.mongoose) {
 }
 
 export async function connectDB() {
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI environment variable is not set");
+  }
+
   if (global.mongoose.conn) {
     return global.mongoose.conn;
   }
 
   if (!global.mongoose.promise) {
     global.mongoose.promise = mongoose
-      .connect(MONGODB_URI, {
-        bufferCommands: false,
-      })
+      .connect(MONGODB_URI, { bufferCommands: false })
       .then((m) => m.connection);
   }
 
